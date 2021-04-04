2aeff58911424291d9b8795a3ce298b9a5a26c63
song <johyesong8686@gmail.com>
2021-04-05 00:02:11 +0900

    feat:페이지 네이션 UI구현

---
+++ component/common/PageNation.jsx
@@ -0,0 +1,104 @@
+import styles from '@/styles/common.module.scss';
+import classnames from 'classnames'
+import Link from 'next/link';
+const PageNation = ({ curPage, startPage, rowsPerPage }) => {
+
+    return (
+        <div className={styles.paging_wrap}>
+            <div className={styles.page_move_pc}>
+                <Link href="/">
+                    <span className={styles.link}> 맨앞 </span>
+                </Link>
+                <Link href="/">
+                    <span className={styles.link}> {'<'} 이전</span>
+                </Link>
+                {
+                    [...Array(rowsPerPage)].map((n, index) => {
+                        return (
+                            curPage === index ?
+                                (
+                                    <Link href="/">
+
+                                        <span className={styles.link}>
+                                            <strong>
+
+                                                {index}
+                                            </strong>
+
+                                        </span>
+
+                                    </Link>
+                                ) : (
+                                    <Link href="/">
+                                        <span className={styles.link}>{index} </span>
+                                    </Link>
+
+                                )
+
+                        )
+
+                    })
+
+
+                }
+
+                <Link href="/">
+                    <span className={styles.link}>다음 {'>'}</span>
+                </Link>
+                <Link href="/">
+                    <span className={classnames(styles.link)}> 맨뒤 </span>
+                </Link>
+
+            </div>
+
+
+            <div className={styles.page_move_mobile}>
+
+                <Link href="/">
+                    <span className={styles.link}> {'<'} </span>
+                </Link>
+                {
+                    [...Array(rowsPerPage)].map((n, index) => {
+                        return (
+                            curPage === index ?
+                                (
+                                    <Link href="/">
+
+                                        <span className={styles.link}>
+                                            <strong>
+
+                                                {index}
+                                            </strong>
+
+                                        </span>
+
+                                    </Link>
+                                ) : (
+                                    <Link href="/">
+                                        <span className={styles.link}>{index} </span>
+                                    </Link>
+
+                                )
+
+                        )
+
+                    })
+
+
+                }
+
+                <Link href="/">
+                    <span className={styles.link}> {'>'}</span>
+                </Link>
+
+            </div>
+
+
+
+        </div>
+
+
+    )
+}
+
+export default PageNation;
