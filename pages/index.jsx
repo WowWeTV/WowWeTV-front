cf9a88747563a6bc0646de94c9f696ce7496e61c
song <johyesong8686@gmail.com>
2021-04-06 00:26:31 +0900

    feat: 메인 header 적용

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,17 @@
 import Head from 'next/head';
 import HomeLayout from '@/component/layout/home';
 import { useSelector } from 'react-redux';
+import HeaderLayout from '@/component/layout/header';
 
 const Main = () => {
   return (
     <div>
       <Head>
         <title>Wow WeTV</title>
-        <meta name="description" content="와우 위티비에 오신걸 환영합니다."></meta>
+        <meta name="description" content="와우 위티비에 오신걸 환영합니다." />
       </Head>
       <>
+        <HeaderLayout />
         <HomeLayout />
       </>
     </div>
