3252ef3efacf6e68f6a9e5beb1283465c7dfe677
song <johyesong8686@gmail.com>
2021-04-06 00:24:37 +0900

    feat: Sass ssr 적용 셋팅

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,19 +1,44 @@
-// import Document, { Html, Head, Main, NextScript } from 'next/document';
-// import '../styles/globals.scss';
+import Document, { Main, NextScript, Head, Html } from 'next/document';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
+class InlineStylesHead extends Head {
+    getCssLinks() {
+        return this.__getInlineStyles();
+    }
 
-// class MyDocument extends Document {
-//   render() {
-//     return (
-//       <Html lang="ko">
-//         <Head />
-//         <body>
-//           <Main />
-//           <NextScript />
-//         </body>
-//       </Html>
-//     );
-//   }
-// }
+    __getInlineStyles() {
+        const { assetPrefix, files } = this.context;
+        console.log(this.context);
+        if (!files || files.length === 0) return null;
 
-// export default MyDocument;
+        return files
+            .filter((file) => /\.css$/.test(file))
+            .map((file) => (
+                <style
+                    key={file}
+                    data-href={`${assetPrefix}/_next/${file}`}
+                    dangerouslySetInnerHTML={{
+                        __html: readFileSync(join(process.cwd(), '.build', file), 'utf-8'),
+                    }}
+                />
+            ));
+    }
+}
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en" dir="ltr">
+                <Head>
+                    <link href="/styles/globals.scss" ref="stylesheets" />
+                    <link href="/styles/search.module.scss" ref="stylesheets" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
