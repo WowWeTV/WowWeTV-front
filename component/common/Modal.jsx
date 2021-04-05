0686b302fde726485140cdebd5c0eb9cf01b674a
Jetty2020 <kmclo@naver.com>
2021-04-05 15:51:16 +0900

    update: modal UI 공통화 작업

--- component/common/Modal.jsx
+++ component/common/Modal.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
-import styles from "@/styles/login.module.scss";
+import styles from "@/styles/modal.module.scss";
 
-const FindPWModal = ({ onHandleModal }) => {
+const FindPWModal = ({
+  onHandleModal,
+  errMsg,
+  header,
+  contentHeader,
+  inputPlaceholder,
+  btnText
+}) => {
   const onClickModal = (e) => {
     e.stopPropagation();
   };
@@ -13,28 +20,26 @@ const FindPWModal = ({ onHandleModal }) => {
   const handleSubmit = () => {
     console.log(inputs);
     if (inputs === "") {
-      setErrorMsg("이메일을 입력해 주세요.");
+      setErrorMsg(`${inputPlaceholder}을 입력해 주세요.`);
     } else {
       //Add function
-      setErrorMsg("가입하지 않은 이메일입니다.");
+      setErrorMsg(errMsg);
     }
   };
   return (
     <div className={styles.modal_wrap} onClick={onHandleModal}>
       <div className={styles.find_modal} onClick={onClickModal}>
         <div className={styles.modal_header}>
-          <h1>비밀번호 찾기</h1>
+          <h1>{header}</h1>
         </div>
         <div>
-          <h2 className={styles.modal_content_header}>
-            찾으려는 계정의 이메일을 입력해 주세요.
-          </h2>
+          <h2 className={styles.modal_content_header}>{contentHeader}</h2>
           <div className={styles.modal_content_wrap}>
             <div className={styles.modal_content}>
               <div className={styles.modal_input}>
                 <input
-                  id="email"
-                  placeholder="이메일"
+                  id="input"
+                  placeholder={inputPlaceholder}
                   type="text"
                   value={inputs}
                   onChange={onChange}
@@ -43,7 +48,7 @@ const FindPWModal = ({ onHandleModal }) => {
               </div>
               {errorMsg && <p>{errorMsg}</p>}
             </div>
-            <button onClick={handleSubmit}>확 인</button>
+            <button onClick={handleSubmit}>{btnText}</button>
           </div>
         </div>
       </div>
