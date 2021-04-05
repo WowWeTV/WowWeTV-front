c3785d4591c684d5ec1c0f49cfc3e316809b3cd9
eunbin-lee <euns2bin@gmail.com>
2021-04-05 18:35:59 +0900

    update: 아이콘 추가 및 사이드메뉴 UI 수정

--- component/layout/header/index.jsx
+++ component/layout/header/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import styles from '@/styles/main.module.scss';
-import Gnb from './Gnb';
-import Lnb from './Lnb';
+import React from "react";
+import styles from "@/styles/main.module.scss";
+import Gnb from "./Gnb";
+import Lnb from "./Lnb";
 
 const HeaderLayout = () => {
-    return (
-        <header className={styles.header}>
-            <Gnb />
-            <Lnb />
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <Gnb />
+      <Lnb />
+    </header>
+  );
 };
 
 export default HeaderLayout;
