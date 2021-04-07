d1e124487abc6717111c851b1c200f05eb12ae30
eunbin-lee <euns2bin@gmail.com>
2021-04-07 19:59:22 +0900

    feat: 메인페이지 'TOP100' UI 구현

--- component/layout/header/Lnb.jsx
+++ component/layout/header/Lnb.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import classnames from 'classnames';
 import styles from '@/styles/layout/header.module.scss';
 
 const Lnb = () => {
   const router = useRouter();
   const { pathname } = router;
+  const [fixed, setFixed] = useState(false);
+  const [pageY, setPageY] = useState(0);
+
+  const handleScroll = useCallback(() => {
+    const { pageYOffset } = window;
+    pageYOffset >= '43' ? setFixed(true) : setFixed(false);
+    setPageY(pageYOffset);
+  }, [fixed, pageY]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [pageY]);
 
   return (
     <>
       {/* mobile, tablet */}
-      <nav className={styles.mobile_lnb_container}>
+      <nav
+        className={
+          fixed
+            ? classnames(styles.mobile_lnb_container, styles.header_fixed)
+            : styles.mobile_lnb_container
+        }
+      >
         <ul>
           <li className={pathname === '/' ? styles.menu_active : null}>
             <h2>
