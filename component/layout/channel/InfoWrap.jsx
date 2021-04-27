f843a7348ac6d7789353bc65506efe6a25ade9f4
song <johyesong8686@gmail.com>
2021-04-27 22:01:49 +0900

    update: 유저 영상 탭 css 수정

--- component/layout/channel/InfoWrap.jsx
+++ component/layout/channel/InfoWrap.jsx
@@ -1,5 +1,4 @@
 import styles from '@/styles/layout/channel.module.scss';
-import styleSearch from '@/styles/layout/search.module.scss';
 import classNames from 'classnames';
 import { useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
@@ -9,8 +8,10 @@ import { AiOutlineShareAlt } from 'react-icons/ai';
 const InfoWrap = () => {
   const { singlechannel } = useSelector((state) => state.user);
   const router = useRouter();
-  const { userId } = router.query;
-  console.log(singlechannel);
+  const { userId, type } = router.query;
+  console.log(userId, type);
+  console.log(type === undefined);
+  console.log(type === 'videos');
   return (
     <div className={styles.inf_wrap}>
       <div className={styles.info_banner}>
@@ -50,11 +51,25 @@ const InfoWrap = () => {
           </div>
         </div>
       </div>
+
       <div className={styles.info_menu}>
         <ul>
-          <li className={styles.selected}>
+          <li
+            className={classNames({
+              [`${styles.selected}`]: type === undefined,
+            })}
+          >
             <Link href={`/user/${userId}`}>
               <span className={styles.span_tab}> 전체</span>
+            </Link>
+          </li>
+          <li
+            className={classNames({
+              [`${styles.selected}`]: type === 'videos',
+            })}
+          >
+            <Link href={`/user/${userId}?type=videos`}>
+              <span className={styles.span_tab}>동영상</span>
             </Link>
           </li>
         </ul>
