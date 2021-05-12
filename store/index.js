7dc4ec7f588c17a22738459a845741a291ede306
Jetty2020 <kmclo@naver.com>
2021-05-12 16:04:58 +0900

    update: 회원가입 기능 서버 연결 완료

--- store/index.js
+++ store/index.js
@@ -1,10 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
 import reducer from '../lib/slices';
 
 const makeStore = (context) =>
   configureStore({
     reducer,
+    middleware: getDefaultMiddleware({
+      serializableCheck: false,
+    }),
   });
 
 const wrapper = createWrapper(makeStore, {
